36bac94f6ee3204bdc198a3655fb78aa2e47a0da
Jeffery Zhao <zzy8200@gmail.com>
2016-11-07 20:08:57 -0800

    Update predict.m

--- HOG_SVR/predict.m
+++ HOG_SVR/predict.m
@@ -1,15 +1,18 @@
 img=imread('F:\b.jpg');
-[y,~,~,~]=rgb2yuv(img);
+[y,~,~,~]=rgb2yuv(imresize(img,[256,256]));
 X=feature_extract_y(y);
-u=predict(mdl_u,X);
-v=predict(mdl_v,X);
-U=double(zeros(256,256));
-V=double(zeros(256,256));
-for i=1:256
-    for j=1:256
-        U(i,j)=u((i-1)*256+j,1);
-        V(i,j)=v((i-1)*256+j,1);
+u_p=predict(mdl_u,X);
+v_p=predict(mdl_v,X);
+u_pp=double(zeros(64,64));
+v_pp=double(zeros(64,64));
+for i=1:64
+    for j=1:64
+        u_pp(i,j)=u_p((i-1)*64+j);
+        v_pp(i,j)=v_p((i-1)*64+j);
     end
 end
-[~,~,~,imgn]=yuv2rgb(y,U,V);
-imshow(imgn)
+
+u_p=scale_matrix(u_pp,4);
+v_p=scale_matrix(v_pp,4);
+[~,~,~,pimg]=yuv2rgb(y,u_p,v_p);
+imshow(pimg);
