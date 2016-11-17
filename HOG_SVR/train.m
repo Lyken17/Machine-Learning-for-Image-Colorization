b4c45e5000fc4d66f388448b15c416d44359e05f
Jeffery Zhao <zzy8200@gmail.com>
2016-11-17 13:47:03 -0800

    limit train size

--- HOG_SVR/train.m
+++ HOG_SVR/train.m
@@ -3,6 +3,7 @@ files=dir('train2014/*.jpg');
 X_F=[];
 U_F=[];
 V_F=[];
+count = 0;
 for file = files'
     img=imread(strcat('train2014/',file.name));
     if size(img,3) < 3
@@ -13,7 +14,12 @@ for file = files'
     X_F=[X_F;X];
     U_F=[U_F;U];
     V_F=[V_F;V];
+    count = count+1
+    if count > 10000
+        break;
+    end
 end
 mdl_u=fitrlinear(X_F,U_F);
 mdl_v=fitrlinear(X_F,V_F);
+
 save('model.mat','mdl_u','mdl_v');
